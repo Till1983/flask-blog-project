6a36029379ff21613e9513bc0a4a08a9d44b66b1
Till1983 <till_ermold@web.de>
2024-04-20 00:06:39 +0200

    restore from previous commit

--- app/app.py
+++ app/app.py
@@ -6,3 +6,17 @@ app.config.from_object('app.config')
 
 app.register_blueprint(blogposts.routes.blueprint)
 #app.register_blueprint(simple_pages.routes.blueprint)
+
+@app.route("/")
+@app.route("/home")
+@app.route("/index")
+def index():
+    return render_template("index.html", title="Welcome")
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title="About this blog")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html", title="Contact me")
