0c2baaca3443a4385536cf8d485ed5e8ce10ef7a
Till1983 <till_ermold@web.de>
2024-04-19 21:24:43 +0200

    create app folder, move files there

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('app.config')
 
 @app.route("/")
 @app.route("/home")
@@ -31,5 +31,3 @@ def post(post_id):
         return render_template("error.html", title="Error")
 
 
-if __name__ == "__main__":
-    app.run()
