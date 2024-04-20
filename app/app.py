f38b12630633699ac0e787a02c80a2e61fe9d6cf
Till1983 <till_ermold@web.de>
2024-04-20 22:12:05 +0200

    import db, create register_extensions

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, redirect, url_for, render_template
 from . import blogposts, users, main_pages
+from app.extensions.database import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config')
+    register_extensions(app)
     register_blueprints(app)
     return app
 
@@ -11,3 +13,6 @@ def register_blueprints(app: Flask):
     app.register_blueprint(blogposts.routes.blueprint)
     app.register_blueprint(users.routes.blueprint)
     app.register_blueprint(main_pages.routes.blueprint)
+
+def register_extensions(app: Flask):
+    db.init_app(app)
