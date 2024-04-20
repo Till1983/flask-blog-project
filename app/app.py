9481d11a610893189d9d35c9327601ce3dcaad75
Till1983 <till_ermold@web.de>
2024-04-20 10:58:45 +0200

    add factory pattern for app and blueprints

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, redirect, url_for, render_template
 from . import blogposts, users, main_pages
 
-app = Flask(__name__)
-app.config.from_object('app.config')
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config')
+    register_blueprints(app)
+    return app
 
-app.register_blueprint(blogposts.routes.blueprint)
-app.register_blueprint(users.routes.blueprint)
-app.register_blueprint(main_pages.routes.blueprint)
+def register_blueprints(app: Flask):
+    app.register_blueprint(blogposts.routes.blueprint)
+    app.register_blueprint(users.routes.blueprint)
+    app.register_blueprint(main_pages.routes.blueprint)
