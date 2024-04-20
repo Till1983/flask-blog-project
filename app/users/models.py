dd3c0afc4cc34882e68e853f9d135a1ea6568f40
Till1983 <till_ermold@web.de>
2024-04-20 22:09:58 +0200

    add Author model

---
+++ app/users/models.py
@@ -0,0 +1,7 @@
+from app.extensions.database import db
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique=True)
+    #article_id = db.Column(db.Integer, db.ForeignKey('article.id'))
