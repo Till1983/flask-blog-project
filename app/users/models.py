9789d8515d3883c497f05072c93945d04a4b04c1
Till1983 <till_ermold@web.de>
2024-04-21 16:35:44 +0200

    add password column to author model

--- app/users/models.py
+++ app/users/models.py
@@ -4,4 +4,5 @@ class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     email = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80), unique=True)
     #article_id = db.Column(db.Integer, db.ForeignKey('article.id'))
