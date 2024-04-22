b8b4bf2b8a0cce58eac07a827949273ea0b21498
Till1983 <till_ermold@web.de>
2024-04-22 22:31:38 +0200

    add names to foreign keys

--- app/blogposts/models.py
+++ app/blogposts/models.py
@@ -4,12 +4,12 @@ from datetime import datetime
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String)
-    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id', name='article_topic'))
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id', name='article_author'))
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     topics = db.relationship('Topic', backref='author', lazy=True)
 
 class Topic(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    article_id = db.Column(db.Integer, db.ForeignKey('article.id'))
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id', name='topic_article'))
