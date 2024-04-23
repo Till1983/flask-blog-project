81ba0e682ac49987266c56d665ceba7354bcfff8
Till1983 <till_ermold@web.de>
2024-04-23 13:55:16 +0200

    add autoincrement to id

--- app/blogposts/models.py
+++ app/blogposts/models.py
@@ -2,16 +2,16 @@ from app.extensions.database import db
 from datetime import datetime
 
 class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(50))
-    content = db.Column(db.String)
+    content = db.Column(db.String(10000))
     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id', name='article_topic'))
     author_id = db.Column(db.Integer, db.ForeignKey('author.id', name='article_author'))
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     topics = db.relationship('Topic', secondary='article_topic', backref='articles', lazy=True)
 
 class Topic(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80))
 
 # Define association table for the many-to-many relationship between Article and Topic
