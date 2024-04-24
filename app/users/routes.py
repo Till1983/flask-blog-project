ece180d1522ceea7f91ff45d8fd9bc893f63824f
Till1983 <till_ermold@web.de>
2024-04-24 16:49:02 +0200

    import flash

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from .models import Author
 from app.extensions.database import db, migrate
 from werkzeug.security import generate_password_hash, check_password_hash
