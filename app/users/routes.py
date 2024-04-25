e30b33d5cb7f34c3a79c67af62faf25ce87086af
Till1983 <till_ermold@web.de>
2024-04-25 12:57:36 +0200

    add get and post blueprints

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, UserMixin
 from .models import Author
 from app.extensions.database import db, migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 
 blueprint = Blueprint('users', __name__)
 
-@blueprint.route('/register', methods=['GET', 'POST'])
+@blueprint.get('/register')
+def show_registration_form():
+    return render_template('register.html', title="Create Your Account Here")
+
+@blueprint.post('/register')
 def create_account():
     if request.method == 'POST':
         name = request.form['name']
@@ -14,14 +18,14 @@ def create_account():
         password = request.form['psw']
         repeat_password = request.form['rpsw']
 
-        # checks if password and repeat_password match
+        # checks if 'password' and 'repeat_password' match
         if password != repeat_password:
             return render_template('register.html', title="Create Your Account Here", error="Password do not match!")
 
         # hashing the password
         hashed_password = generate_password_hash(password)
 
-        # adding new user to the database
+        # adds new user to the database
         new_user = Author(name=name, email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
@@ -30,7 +34,11 @@ def create_account():
 
     return render_template('register.html', title="Create Your Account Here")
 
-@blueprint.route('/login', methods=['GET', 'POST'])
+@blueprint.get('/login')
+def show_login_form():
+    return render_template('login.html', title="Login")
+
+@blueprint.post('/login')
 def login():
     if request.method == 'POST':
         email = request.form['email']
@@ -48,7 +56,7 @@ def login():
 
     return render_template('login.html', title="Login")
 
-@blueprint.route('/logout')
+@blueprint.get('/logout')
 def logout():
     session.clear()
     return render_template('logout.html', title="Until next time!")
