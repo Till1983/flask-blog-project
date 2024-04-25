1aa23bfdd9f14aac5260377762bbd9523430f024
Till1983 <till_ermold@web.de>
2024-04-25 21:04:50 +0200

    delete post route, add create_post route

--- app/blogposts/routes.py
+++ app/blogposts/routes.py
@@ -8,11 +8,6 @@ def posts():
     articles = Article.query.all()
     return render_template("posts.html", title="Thoughts and Musings", articles=articles)
 
-@blueprint.route('/posts/<int:post_id>')
-def post(post_id):
-    '''Returns post by id from "articles" dictionary and shows error page if the post does not exist'''
-    post = articles.get(post_id)
-    if post:
-        return render_template("post.html", post=post)
-    else:
-        return render_template("error.html", title="Error")
+@blueprint.route('/create-post')
+def create_post():
+    return render_template("create-post.html")
