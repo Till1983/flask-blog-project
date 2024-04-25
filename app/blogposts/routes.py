fb60e49e4da7da68ff06624d0e8e9b0eda36c5f5
Till1983 <till_ermold@web.de>
2024-04-26 00:10:12 +0200

    add space between blueprints

--- app/blogposts/routes.py
+++ app/blogposts/routes.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, render_template
 from app.blogposts.models import Article, Topic
 
+
 blueprint = Blueprint('blogposts', __name__)
+
 
 @blueprint.route('/posts')
 def posts():
     articles = Article.query.all()
     return render_template("posts.html", title="Thoughts and Musings", articles=articles)
+
 
 @blueprint.route('/create-post')
 def create_post():
