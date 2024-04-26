c435743ee2d74ad3e31532d3bf1549b0ea3d56cc
Till1983 <till_ermold@web.de>
2024-04-26 12:01:08 +0200

    import current_user, add authentication

--- app/blogposts/routes.py
+++ app/blogposts/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app.blogposts.models import Article, Topic
+from flask_login import login_required, login_user, logout_user, current_user
 
 
 blueprint = Blueprint('blogposts', __name__)
@@ -11,6 +12,22 @@ def posts():
     return render_template("posts.html", title="Thoughts and Musings", articles=articles)
 
 
-@blueprint.route('/create-post')
-def create_post():
+@blueprint.get('/create-post')
+@login_required
+def get_create_post():
+    return render_template("create-post.html")
+
+
+@blueprint.before_request
+def check_login_status():
+    if not current_user.is_authenticated:
+        flash('You need to log in to create a blogpost!', "error")
+        return redirect(url_for('users.show_login_form'))
+
+
+@blueprint.post('/create-post')
+def post_create_post():
+    name = request.form.get['name']
+    title = request.form.get['title']
+    blogpost = request.form.get['blogpost']
     return render_template("create-post.html")
