67372f16bc56a5b94fa7e972b57a935a646161ea
Till1983 <till_ermold@web.de>
2024-04-27 23:21:39 +0200

    add routes for view, edit, delete posts

--- app/blogposts/routes.py
+++ app/blogposts/routes.py
@@ -21,7 +21,7 @@ def get_create_post():
 
 @blueprint.before_request
 def check_login_status():
-    if request.endpoint != 'blogposts.posts' and not current_user.is_authenticated:
+    if request.endpoint not in ['blogposts.posts', 'blogposts.view_post'] and not current_user.is_authenticated:
         return redirect(url_for('users.show_login_form'))
 
 
@@ -32,5 +32,35 @@ def post_create_post():
 
     new_post = Article(author=current_user, title=title, content=content)
     db.session.add(new_post)
+    db.session.commit()
+    return redirect(url_for('blogposts.posts'))
+
+
+@blueprint.route('/post/<int:post_id>')
+def view_post(post_id):
+    article = Article.query.get_or_404(post_id)
+    return render_template('view-post.html', title=article.title, article=article)
+
+
+@blueprint.route('/edit-post/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def edit_post(post_id):
+    article = Article.query.get_or_404(post_id)
+    if article.author != current_user:
+        abort(403)
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('blogposts.posts'))
+    return render_template('edit-post.html', article=article)
+
+
+@blueprint.route('/delete-post/<int:post_id>')
+def delete_post(post_id):
+    article = Article.query.get_or_404(post_id)
+    if article.author != current_user:
+        abort(403)
+    db.session.delete(article)
     db.session.commit()
     return redirect(url_for('blogposts.posts'))
