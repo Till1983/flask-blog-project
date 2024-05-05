b9a783b93aa0c4132791b2e9588715ff747d829a
Till1983 <till_ermold@web.de>
2024-05-06 00:27:04 +0200

    CHANGE: names of functions

--- app/tests/users/test_user_routes.py
+++ app/tests/users/test_user_routes.py
@@ -1,12 +1,12 @@
 # Test login and register pages for correct display
 # The code below has been adapted from https://codecookies.xyz/flask-tutorial/v1/unit-testing and modified.
 
-def test_register_display(client):
+def test_show_registration_form(client):
     '''tests the register page'''
     response = client.get('/register')
     assert response.status_code == 200
 
-def test_register_display(client):
+def test_show_login_form(client):
     '''tests the login page'''
     response = client.get('/login')
     assert response.status_code == 200
