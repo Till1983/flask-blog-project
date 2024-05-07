3f5b4e5a7718d2907493a507ad4e316505f4f4b9
Till1983 <till_ermold@web.de>
2024-05-07 09:45:18 +0200

    CHANGE: rewrite comments

--- app/tests/main_pages/test_main_routes.py
+++ app/tests/main_pages/test_main_routes.py
@@ -2,7 +2,7 @@
 # The code below has been adapted from examples at https://codecookies.xyz/flask-tutorial/v1/unit-testing
 
 def test_index_display(client):
-    '''tests the index page'''
+    '''Test the correct display of the index page'''
     index_urls = ["/", "/index", "/home"]
 
     for url in index_urls:
@@ -10,11 +10,11 @@ def test_index_display(client):
         assert response.status_code == 200
 
 def test_about_display(client):
-    '''tests the about page'''
+    '''Test the correct display of the about page'''
     response = client.get('/about')
     assert response.status_code == 200
 
 def test_contact_display(client):
-    '''tests the contact page'''
+    '''Test the correct display of the contact page'''
     response = client.get('/contact')
     assert response.status_code == 200
