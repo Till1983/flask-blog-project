cfa380eb2e4e8b6f45ad648ac895fdeffc01c58d
Till1983 <till_ermold@web.de>
2024-05-21 21:38:35 +0200

    ALTER: increase title & content character limit

--- app/blogposts/models.py
+++ app/blogposts/models.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(100))
-    content = db.Column(db.String(10000))
+    title = db.Column(db.String(255))
+    content = db.Column(db.String(100000))
     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id', name='article_topic'))
     author_id = db.Column(db.Integer, db.ForeignKey('author.id', name='article_author'))
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
