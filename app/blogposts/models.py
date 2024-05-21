0c22955b895770841d9ea20b5cc321d707957008
Till1983 <till_ermold@web.de>
2024-05-21 21:54:57 +0200

    REMOVE topic and article_topic tables

--- app/blogposts/models.py
+++ app/blogposts/models.py
@@ -5,17 +5,17 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(255))
     content = db.Column(db.String(100000))
-    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id', name='article_topic'))
+    #topic_id = db.Column(db.Integer, db.ForeignKey('topic.id', name='article_topic'))
     author_id = db.Column(db.Integer, db.ForeignKey('author.id', name='article_author'))
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     topics = db.relationship('Topic', secondary='article_topic', backref='articles', lazy=True)
 
-class Topic(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(80))
+#class Topic(db.Model):
+ #   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  #  name = db.Column(db.String(80))
 
 # Define association table for the many-to-many relationship between Article and Topic
-article_topic = db.Table('article_topic',
-    db.Column('article_id', db.Integer, db.ForeignKey('article.id'), primary_key=True),
-    db.Column('topic_id', db.Integer, db.ForeignKey('topic.id'), primary_key=True)
-)
+#article_topic = db.Table('article_topic',
+ #   db.Column('article_id', db.Integer, db.ForeignKey('article.id'), primary_key=True),
+  #  db.Column('topic_id', db.Integer, db.ForeignKey('topic.id'), primary_key=True)
+#)
